38fa2aa5de8469e3ee08a543d3cdf93f1f50362f
lucasmayor <lucasmayor@bluewin.ch>
2020-07-14 10:45:05 +0200

    First Commit

---
+++ OBC_test/hdl/test_SPI_tester.vhd
@@ -0,0 +1,13 @@
+--
+-- VHDL Architecture OBC_test.test.SPI_tester
+--
+-- Created:
+--          by - Lucas.Mayor (DESKTOP-3I0F3HP)
+--          at - 13:14:41 13.07.2020
+--
+-- using Mentor Graphics HDL Designer(TM) 2019.2 (Build 5)
+--
+ARCHITECTURE SPI_tester OF test IS
+BEGIN
+END ARCHITECTURE SPI_tester;
+
